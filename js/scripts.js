040c8fe309df00fa1a3c338400972b34fd2e3f8a
Bacon <cbacon4@vcd-ch204-13.eastern.ewu.edu>
2016-10-06 14:17:17 -0700

    Added Image

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Test scripts.js
+
+console.log('OK USA');
+
+
+// Test Jquery
+//$('img').hide();
+
+
+$('img').imageLens({
+    imageSrc: '/img/watch-zoom.jpg',
+    lensSize: 600,
+    borderColor: "#ffff31",
+    
+    
+});
